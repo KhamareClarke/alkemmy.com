12ca521087ee5ea84194ebc636e381c182f90926
KhamareClarke <khamareclarke@gmail.com>
2025-10-30 00:45:02 +0500

    Fix skin-matcher build error by adding dynamic rendering config

--- app/skin-matcher/page.tsx
+++ app/skin-matcher/page.tsx
@@ -1,7 +1,8 @@
-'use client';
-
-import React from 'react';
 import SkinMatcherQuiz from '@/components/SkinMatcherQuiz';
+
+// Force dynamic rendering - this page should not be statically generated
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default function SkinMatcherPage() {
   return <SkinMatcherQuiz />;
